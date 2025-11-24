7f2a29b68b0fa4ef3451779afc6d672f77370c43
jnielsen713 <143571582+jnielsen713@users.noreply.github.com>
2025-11-24 15:11:01 -0800

    Added API functionality to db_programming.py

--- db_programming.py
+++ db_programming.py
@@ -1,29 +1,73 @@
+# For connection
 import mysql.connector
 from mysql.connector import errorcode
 from dotenv import load_dotenv
 import os
 
+# For API
+from flask import Flask, jsonify, request
+
+
 # This will let us keep our user information private by reading it from an external file.
 load_dotenv()
 
+
 # Try-Except block to print an error message instead of crashing the program
 # From Zybook
-try:
-    youthGroupConnection = mysql.connector.connect(
-        user=os.getenv('DB_USER'),
-        password=os.getenv('DB_PASSWORD'),
-        host=os.getenv('DB_HOST'),
-        port=os.getenv('DB_PORT', 3306),
-        database=os.getenv('DB_NAME'))
+def get_connection():
+    try:
+        youthGroupConnection = mysql.connector.connect(
+            user=os.getenv('DB_USER'),
+            password=os.getenv('DB_PASSWORD'),
+            host=os.getenv('DB_HOST'),
+            port=os.getenv('DB_PORT', 3306),
+            database=os.getenv('DB_NAME'))
 
-except mysql.connector.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print('Invalid credentials')
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print('Database not found')
-    else:
-        print('Cannot connect to database:', err)
+    except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print('Invalid credentials')
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print('Database not found')
+        else:
+            print('Cannot connect to database:', err)
+    
+    return youthGroupConnection
 
-else:
-    # The rest of the code will go here
+
+# API
+app = Flask(__name__)
+
+@app.get("/people")
+def get_people():
+    youthGroupConnection = get_connection()
+    youthGroupCursor = youthGroupConnection.cursor()
+    youthGroupCursor.execute("SELECT firstName, lastName, email, phone FROM Person;")
+    rows = youthGroupCursor.fetchall()
+    youthGroupCursor.close()
     youthGroupConnection.close()
+    return jsonify(rows)
+
+@app.get("/events")
+def get_events():
+    youthGroupConnection = get_connection()
+    youthGroupCursor = youthGroupConnection.cursor()
+    youthGroupCursor.execute("SELECT eventName, startDateTime, location FROM Event;")
+    rows = youthGroupCursor.fetchall()
+    youthGroupCursor.close()
+    youthGroupConnection.close()
+    return jsonify(rows)
+
+@app.get("/smallgroups")
+def get_smallgroups():
+    youthGroupConnection = get_connection()
+    youthGroupCursor = youthGroupConnection.cursor()
+    youthGroupCursor.execute("SELECT name, description FROM SmallGroup;")
+    rows = youthGroupCursor.fetchall()
+    youthGroupCursor.close()
+    youthGroupConnection.close()
+    return jsonify(rows)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# Depending on the port, running http://127.0.0.1:5000/people in Insomnia should yield the proper results.
